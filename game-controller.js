d17d7ba2ea163a04149d0dfd85d65ec40bc802e0
Roi Yehoshua <r.yehoshua@northeastern.edu>
2022-10-08 12:11:14 -0400

    Added game menu

--- game-controller.js
+++ game-controller.js
@@ -4,56 +4,109 @@ import Snake from "./snake.js";
 class GameController {  
   #canvas
   #ctx
+  #scoreLabel
+  #gridSize
   #speed
+  #score  
+  #boardWidth
+  #boardHeight  
   #snake
   #apple
   #timer
+  #allowPressKeys
 
   /**
    * 
    * @param {HTMLCanvasElement} canvas 
+   * @param {HTMLSpanElement} scoreLabel
    * @param {*} gridSize 
    * @param {*} speed 
    */
-  constructor(canvas, gridSize = 15, speed = 100) {  
+  constructor(canvas, scoreLabel, gridSize = 15, speed = 100) {  
     this.#canvas = canvas;  
-    this.#ctx = canvas.getContext('2d');    
-    this.#speed = speed;
-
-    const boardWidth = Math.floor(canvas.width / gridSize);
-    const boardHeight = Math.floor(canvas.height / gridSize);
-
-    this.#snake = new Snake(this.#ctx, gridSize, boardWidth, boardHeight); 
-    this.#apple = new Apple(this.#ctx, gridSize, boardWidth, boardHeight);
-    this.#apple.chooseRandomPosition(this.#snake);
-
-    this.#handleKeyboardEvents();
+    this.#ctx = canvas.getContext('2d');
+    this.#scoreLabel = scoreLabel;
+    this.#gridSize = gridSize;
+    this.#speed = speed;    
+    this.#boardWidth = Math.floor(canvas.width / gridSize);
+    this.#boardHeight = Math.floor(canvas.height / gridSize);    
   }
 
-  runGame() {
-    this.#timer = setInterval(() => {
-      this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
-      this.#snake.draw();
+  startGame() {
+    this.#snake = new Snake(this.#ctx, this.#gridSize, this.#boardWidth, this.#boardHeight); 
+    this.#apple = new Apple(this.#ctx, this.#gridSize, this.#boardWidth, this.#boardHeight);
+    this.#apple.chooseRandomPosition(this.#snake);
 
-      const moveType = this.#snake.move(this.#apple);
-      switch (moveType) {
-        case Snake.MoveType.INVALID:
-          clearInterval(this.#timer);
-          alert('Game over!');
-          break;
-        case Snake.MoveType.APPLE_EATEN:
-          this.#apple.chooseRandomPosition(this.#snake);
-          break;
-      }       
-      this.#apple.draw(); 
+    this.#snake.draw();
+    this.#apple.draw(); 
+    this.#handleKeyboardEvents();
+    this.#resetScore();
+    
+    this.#timer = setInterval(() => { 
+      this.#runGame(); 
     }, this.#speed);  
-  }  
+    this.#allowPressKeys = true;
+  }
+
+  #runGame() {
+    this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
+
+    const moveType = this.#snake.move(this.#apple);
+    switch (moveType) {
+      case Snake.MoveType.INVALID:
+        this.#gameOver();          
+        return;        
+      case Snake.MoveType.APPLE_EATEN:
+        this.#apple.chooseRandomPosition(this.#snake);
+        this.#updateScore();
+        break;
+    }       
+
+    this.#snake.draw();
+    this.#apple.draw(); 
+  }
+
+  restartGame() {
+    this.pauseGame();       
+    this.startGame();
+  }
+
+  pauseGame() {
+    clearInterval(this.#timer);
+    this.#allowPressKeys = false;
+  }
+
+  resumeGame() {
+    this.#timer = setInterval(() => { 
+      this.#runGame(); 
+    }, this.#speed);
+    this.#allowPressKeys = true;
+  }
 
   #handleKeyboardEvents() {
     document.addEventListener('keydown', e => {
+      if (!this.#allowPressKeys) {
+        return;
+      }
       this.#snake.changeDirection(e.key);
     });
   }
+
+  #updateScore() {
+    this.#score++;
+    this.#scoreLabel.textContent = this.#score;
+  }
+
+  #gameOver() {
+    clearInterval(this.#timer);    
+    alert(`Game over. Your score was ${this.#score}`);
+    this.#resetScore();
+  }
+
+  #resetScore() {
+    this.#score = 0;
+    this.#scoreLabel.textContent = this.#score;
+  }  
 }
 
 export default GameController;
--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 import GameController from "./game-controller.js";
 
 const canvas = document.getElementById('canvas');
+const btnPlay = document.getElementById('btnPlay');
+const btnRestart = document.getElementById('btnRestart');
+const btnPause = document.getElementById('btnPause');
+const btnResume = document.getElementById('btnResume');
+const scoreLabel = document.getElementById('score');
 
-const controller = new GameController(canvas);
-controller.runGame();
+const controller = new GameController(canvas, scoreLabel);
+
+btnPlay.addEventListener('click', () => {
+  controller.startGame();
+  btnPlay.hidden = true;
+  btnRestart.hidden = false;
+});
+
+btnRestart.addEventListener('click', () => {
+  controller.restartGame();
+});
+
+btnPause.addEventListener('click', () => {
+  controller.pauseGame();
+  btnPause.hidden = true;
+  btnResume.hidden = false;
+});
+
+btnResume.addEventListener('click', () => {
+  controller.resumeGame();
+  btnPause.hidden = false;
+  btnResume.hidden = true;
+});
+
 
